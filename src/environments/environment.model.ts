7a47f1d9f1281579ad0360796471d57fae52de64
srini_947 <srinivasa.keshavamurthy@chubb.com>
2021-09-07 11:06:01 +0800

    Auto-generated commit [ci skip]

--- src/environments/environment.model.ts
+++
@@ -1,5 +0,0 @@
-export interface EnvironmentModel {
-  production: boolean;
-  API_KEY: string;
-  CLIENT_SECRET: string;
-}
--- src/environments/environment.prod.ts
+++
@@ -1,7 +0,0 @@
-import { EnvironmentModel } from './environment.model';
-
-export const environment: EnvironmentModel = {
-  production: true,
-  API_KEY: 'dev_test',
-  CLIENT_SECRET: '3H1Bf6mCctIgpCuzvrnyekf3VhAUEnKJ',
-};
